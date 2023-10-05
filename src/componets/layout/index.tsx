2fa1da8e4dce01b4c5a182c2ba19470fe781d735
Arun Prabhakaran <arunpraba.k@gmail.com>
2023-10-05 08:41:28 +0530

    Layout and api added

---
+++ src/componets/layout/index.tsx
@@ -0,0 +1,24 @@
+import { Outlet } from 'react-router-dom'
+
+export default function Layout() {
+  return (
+    <>
+      <nav className="bg-slate-800">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 items-center justify-between">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <p className="font-bold text-inherit">Cricket Players</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <main>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          <Outlet />
+        </div>
+      </main>
+    </>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+const Layout = React.lazy(() => import('./componets/layout'))
 const Players = React.lazy(() => import('./pages/players'))
 const PlayerDetail = React.lazy(() => import('./pages/player-detail'))
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Players />,
-  },
-  {
-    path: '/:playerId',
-    element: <PlayerDetail />,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Players />,
+      },
+      {
+        path: '/:playerId',
+        element: <PlayerDetail />,
+      },
+    ],
   },
 ])
 
